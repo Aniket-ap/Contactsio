e93f01f4e6a16c8c699e7983aab58fd4c42f1af2
Aniket Pradhan <aniket_pro@outlook.com>
2020-07-17 23:11:24 +0530

    frontend

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className='my-1'>
+      Contactsio is a free personal information manager to store contact information about your friends, family, and colleges in one database file.
+      </p>
+      <p className='bg-dark p'>
+        <strong>Version: </strong> 1.0.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
